411422dae45d696c2cd89d48bd73ae5de5f6c35c
Atianna DeAngelo <atianna.deangelo@gmail.com>
2021-06-10 15:02:12 -0600

    ‘init

---
+++ app/controllers/api/doctors_controller.rb
@@ -0,0 +1,51 @@
+class Api::DoctorsController < ApplicationController
+  before_action :set_doctor, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    doctors = Doctor.all
+    render json: doctors
+  end
+
+  def show
+  end
+
+  def new
+    @doctor = Doctor.new
+  end
+  
+  def create
+    @doctor = Doctor.new(doctor_params)
+
+    if @doctor.save
+      render json: @doctor
+    else
+      render json: {error: @doctor.errors}, status: 422
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @doctor.update(doctor_params)
+      render json: @doctor
+    else
+      render json: edit
+    end
+  end
+
+  def destroy
+    @doctor.destroy
+    render json: @doctor
+  end
+
+  private
+    def set_doctor
+      @doctor = Doctor.find(params[:id])
+    end
+
+    def doctor_params
+      params.require(:doctor).permit(:name, :specialty)
+    end
+end
+
---
+++ db/seeds.rb
@@ -0,0 +1,28 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+users = []
+doctors = []
+
+30.times do |i|
+  users << User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+end
+
+3.times do |i|
+  doctors << Doctor.create(last_name: Faker::TvShows::BigBangTheory.character, specialty: Faker::Science.element_subcategory)
+end
+
+10.times do
+  doctor.appointments.create(date: Faker::Date.between(from: '2020-10-23', to: '2020-12-31'), user_id: users.sample.id)
+  end
+end
+
+puts "seeded"
+puts Doctor.all.size
+puts User.all.size
+puts Appointment.all.size
